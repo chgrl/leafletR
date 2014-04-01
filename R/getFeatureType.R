cf7bebe6c854e9bf0f076d74d5576d3e914e5a7a
chgrl <christian.graul@gmail.com>
2014-04-01 12:47:33 +0200

    get feature type using RJSONIO

--- R/getFeatureType.R
+++ R/getFeatureType.R
@@ -1,26 +1,39 @@
 getFeatureType <-
 function(dat) {
 	# check if file exists
-	if(!file.exists(dat)) stop("data file not found")
+	if(!file.exists(dat)) stop("data file not found\n")
 	
-	con <- file(dat, "rt") 
+	suppressWarnings(rjsonio <- require(RJSONIO, quietly=TRUE))
 	
-	ft <- NULL
-	for(l in 1:100) {
-		line <- readLines(con, 1)
-		if(length(line)==0) break
-		if(length(grep("\"type\":\"Point\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0 || length(grep("\"type\":\"MultiPoint\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0) {
-			ft <- "point"
-			break
-		} else if(length(grep("\"type\":\"LineString\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0 || length(grep("\"type\":\"MultiLineString\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0) {
-			ft <- "line"
-			break
-		} else if(length(grep("\"type\":\"Polygon\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0 || length(grep("\"type\":\"MultiPolygon\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0) {
-			ft <- "polygon"
-			break
+	if(rjsonio) {	# RJSONIO package available
+		fl <- fromJSON(dat)
+		ft <- fl$features[[1]]$geometry$type
+  		if(length(fl$features)>1) for(n in 2:length(fl$features)) if(fl$features[[n]]$geometry$type!=ft) stop("GeoJSON contains different geometry types\n")
+  		if(ft=="Point" || ft=="MultiPoint") ft <- "point"
+  		else if(ft=="LineString" || ft=="MultiLineString") ft <- "line"
+  		else if(ft=="Polygon" || ft=="MultiPolygon") ft <- "polygon"
+  		else stop("geometry type not recognized\n")
+	} else {	# fallback if RJSONIO package not available
+		con <- file(dat, "rt") 
+		
+		ft <- NULL
+		for(l in 1:100) {
+			line <- readLines(con, 1)
+			if(length(line)==0) break
+			if(length(grep("\"type\":\"Point\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0 || length(grep("\"type\":\"MultiPoint\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0) {
+				ft <- "point"
+				break
+			} else if(length(grep("\"type\":\"LineString\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0 || length(grep("\"type\":\"MultiLineString\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0) {
+				ft <- "line"
+				break
+			} else if(length(grep("\"type\":\"Polygon\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0 || length(grep("\"type\":\"MultiPolygon\"", gsub(" ","", line, fixed=TRUE), ignore.case=TRUE))!=0) {
+				ft <- "polygon"
+				break
+			}
+			else stop("geometry type not recognized\n")
 		}
+		
+		close(con)
 	}
-	
-	close(con)
 	return(ft)
 }
