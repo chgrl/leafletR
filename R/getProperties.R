c5ee4e721fbfe1e4d185518dae92b2af59a17df5
chgrl <christian.graul@gmail.com>
2014-04-02 11:52:44 +0200

    getProperties not internal any more

--- R/getProperties.R
+++ R/getProperties.R
@@ -1,16 +1,17 @@
 getProperties <-
-function(dat) {
-	stopifnot(require(RJSONIO))
+function(data, print=TRUE) {
+	stopifnot(require(RJSONIO, quietly=TRUE))
 	
 	# check if file exists and validate GeoJSON
-	if(!file.exists(dat)) stop("data file not found")	
-	if(!isValidJSON(dat)) stop("invalid GeoJSON")
+	if(!file.exists(data)) stop("data file not found")	
+	if(!isValidJSON(data)) stop("invalid GeoJSON")
 	
 	# get properties
-	json <- fromJSON(dat)
+	json <- fromJSON(data)
 	prop <- NULL
 	for(n in 1:length(json$features)) prop <- append(prop, names(json$features[[n]]$properties))
 	prop <- unique(prop)
-		
-	return(prop)
+	
+	if(print) print(prop)
+	invisible(prop)
 }
