40fb3335a5393aa4383932feeabd1b67b5dddb8a
chgrl <christian.graul@gmail.com>
2014-05-28 12:57:31 +0200

    ignore non-used arguments

--- R/styleSingle.R
+++ R/styleSingle.R
@@ -7,13 +7,13 @@ function(col, lwd, alpha, fill, fill.alpha, rad) {
 		if(is.na(col)) style <- append(style, "stroke: false")
 		else style <- append(style, paste("color: \"", getHex(col), "\"", sep=""))
 	}
-	if(!missing(lwd)) style <- append(style, paste("weight:", lwd))
-	if(!missing(alpha)) style <- append(style, paste("opacity:", alpha))
+	if(!missing(lwd) && !missing(col)) if(!is.na(col)) style <- append(style, paste("weight:", lwd))
+	if(!missing(alpha) && !missing(col)) if(!is.na(col)) style <- append(style, paste("opacity:", alpha))
 	if(!missing(fill)) {
 		if(is.na(fill)) style <- append(style, "fill: false")
 		else style <- append(style, paste("fillColor: \"", getHex(fill), "\"", sep=""))
 	}
-	if(!missing(fill.alpha)) style <- append(style, paste("fillOpacity:", fill.alpha))
+	if(!missing(fill.alpha) && !missing(fill)) if(!is.na(fill)) style <- append(style, paste("fillOpacity:", fill.alpha))
 	if(!missing(rad)) style <- append(style, paste("radius:", rad))
 
 	if(is.null(style)) stop("No style parameters defined")
