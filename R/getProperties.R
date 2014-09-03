833ce8869f353b9736db165606c6cd89d63a6207
chgrl <christian.graul@gmail.com>
2014-09-03 17:55:29 +0200

    Revert b8a5e1d..27b4f2c
    This rolls back to commit b8a5e1d03d59a5c7dbf28e405e6e2d4ac0ab76a6.

--- R/getProperties.R
+++ R/getProperties.R
@@ -1,15 +1,16 @@
 getProperties <-
 function(data, print=TRUE) {
 	
-	# check if file exists and convert GeoJSON
-	if(!file.exists(data)) stop("Data file not found")
-	json <- jsonlite::fromJSON(data)
-	#the following drops an error, but why?
-	#tryCatch(json <- jsonlite::fromJSON(data), error=stop("Invalid GeoJSON", call.=FALSE))
+	# check if file exists and validate GeoJSON
+	if(!file.exists(data)) stop("Data file not found")	
+	if(!isValidJSON(data)) stop("Invalid GeoJSON")
 	
 	# get properties
-	prop <- unique(names(json$features$properties))
-	cat(prop)
+	json <- fromJSON(data)
+	prop <- NULL
+	for(n in 1:length(json$features)) prop <- append(prop, names(json$features[[n]]$properties))
+	prop <- unique(prop)
+	
 	if(print) print(prop)
 	invisible(prop)
 }
--- R/leaflet.R
+++ R/leaflet.R
@@ -3,15 +3,11 @@ function(data, dest, title, size, base.map="osm", center, zoom, style, popup, in
 	if(missing(data)) data <- NA
 	if(length(data)>1) for(n in 1:length(data)) {
 		if(!is.na(data[[n]])) if(tolower(tail(strsplit(tail(strsplit(data[[n]], "/")[[1]], 1), "[.]")[[1]], 1))!="geojson") stop("'data' requires GeoJSON files (file extension should be 'geojson')")
-		json <- jsonlite::fromJSON(data[[n]])  # just for testing
-		#the following drops an error, but why?
-		#tryCatch(json <- fromJSON(data[[n]]), error=stop("'data' contains invalid JSON file", call.=FALSE))
+		if(!isValidJSON(data[[n]])) stop("'data' is not a valid JSON file")
 	} else {
 		if(!is.na(data)) {
 			if(tolower(tail(strsplit(tail(strsplit(data, "/")[[1]], 1), "[.]")[[1]], 1))!="geojson") stop("'data' requires GeoJSON files (file extension should be 'geojson')")
-			json <- jsonlite::fromJSON(data)  # just for testing
-			#the following drops an error, but why?
-			#tryCatch(json <- jsonlite::fromJSON(data), error=stop("'data' is not a valid JSON file", call.=FALSE))
+			if(!isValidJSON(data)) stop("'data' is not a valid JSON file")
 		}
 	}
 	if(missing(dest)) dest <- getwd()
