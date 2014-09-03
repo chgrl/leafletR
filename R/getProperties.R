18894fb89bef9e9a84d2e9ebd7099ac1f51f8068
chgrl <christian.graul@gmail.com>
2014-09-03 10:20:54 +0200

    JSON validation fixed

--- R/getProperties.R
+++ R/getProperties.R
@@ -1,12 +1,11 @@
 getProperties <-
 function(data, print=TRUE) {
 	
-	# check if file exists and validate GeoJSON
-	if(!file.exists(data)) stop("Data file not found")	
-	if(!validate(data)) stop("Invalid GeoJSON")
+	# check if file exists and convert GeoJSON
+	if(!file.exists(data)) stop("Data file not found")
+	tryCatch(json <- fromJSON(data), error=stop("Invalid GeoJSON", call.=FALSE))
 	
 	# get properties
-	json <- fromJSON(data)
 	prop <- NULL
 	for(n in 1:length(json$features)) prop <- append(prop, names(json$features[[n]]$properties))
 	prop <- unique(prop)
--- R/leaflet.R
+++ R/leaflet.R
@@ -3,11 +3,11 @@ function(data, dest, title, size, base.map="osm", center, zoom, style, popup, in
 	if(missing(data)) data <- NA
 	if(length(data)>1) for(n in 1:length(data)) {
 		if(!is.na(data[[n]])) if(tolower(tail(strsplit(tail(strsplit(data[[n]], "/")[[1]], 1), "[.]")[[1]], 1))!="geojson") stop("'data' requires GeoJSON files (file extension should be 'geojson')")
-		if(!validate(data[[n]])) stop("'data' is not a valid JSON file")
+		tryCatch(json <- fromJSON(data[[n]]), error=stop("'data' contains invalid JSON file", call.=FALSE))
 	} else {
 		if(!is.na(data)) {
 			if(tolower(tail(strsplit(tail(strsplit(data, "/")[[1]], 1), "[.]")[[1]], 1))!="geojson") stop("'data' requires GeoJSON files (file extension should be 'geojson')")
-			if(!validate(data)) stop("'data' is not a valid JSON file")
+			tryCatch(json <- fromJSON(data), error=stop("'data' is not a valid JSON file", call.=FALSE))
 		}
 	}
 	if(missing(dest)) dest <- getwd()
