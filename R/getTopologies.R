a4f5f1b3a4c7c5214937deb37a00cf792857668a
chgrl <christian.graul@gmail.com>
2014-09-30 11:33:44 +0200

    name changed

---
+++ R/getTopologies.R
@@ -0,0 +1,19 @@
+getTopologies <-
+function(data, print=TRUE) {
+	
+	# if file path is given
+	if(is.character(data)) {
+		# check if file exists and convert JSON
+		if(!file.exists(data)) stop("Data file not found")
+		data <- jsonlite::fromJSON(data)
+		if(is.null(data$type)) stop("'data' requires TopoJSON file")
+		if(tolower(data$type)!="topology") stop("'data' requires TopoJSON file")
+	}
+	
+	# get objects
+	obj <- unique(names(data$objects))
+	
+	# print and return
+	if(print) print(obj)
+	invisible(obj)
+}
