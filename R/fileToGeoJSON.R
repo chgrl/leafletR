79f822a1b7f0c31fdec08c3dd35dedd38add126f
chgrl <christian.graul@gmail.com>
2015-02-03 12:06:51 +0100

    missing quotes added

--- R/fileToGeoJSON.R
+++ R/fileToGeoJSON.R
@@ -7,7 +7,7 @@ function(data, name, dest, overwrite) {
 	# taken from rgbif package: cran.r-project.org/package=rgbif‎
 	# package import impractical, since rgbif imports several other packages
 	url <- "http://ogre.adc4gis.com/convert"
-	tt <- httr::POST(url, body=list(upload=httr::upload_file(data), skipFailures=yes))
+	tt <- httr::POST(url, body=list(upload=httr::upload_file(data), skipFailures="yes"))
 	out <- httr::content(tt, as="text")
 	fileConn <- file(paste0(file.path(dest, name), ".geojson"))
 	writeLines(out, fileConn)
