613f59743597eb87c06ab52ffbbd908260041f01
chgrl <christian.graul@gmail.com>
2015-04-21 20:04:35 +0200

    trying to fix the factor bug

--- R/dfToGeoJSON.R
+++ R/dfToGeoJSON.R
@@ -26,12 +26,12 @@ function(data, name, dest, lat.lon, overwrite) {
 			cat("      \"properties\": {", file=path, append=TRUE, sep="\n")
 			dat <- data[f,-lat.lon]
 			if(!is.data.frame(dat)) names(dat) <- names(data)[-lat.lon]
-			
+				
 			if(length(dat)==1) {
 				cat(paste0("        \"", names(data)[-lat.lon], "\": \"", dat, "\"\n"), file=path, append=TRUE)
 			} else {
-				for(p in 1:length(dat)) {
-					cat(paste0("        \"", names(dat)[p], "\": \"", dat[p], "\""), file=path, append=TRUE)
+				for(p in 1:length(dat)) {	
+					cat(paste0("        \"", names(dat)[p], "\": \"", as.character(dat[p]), "\""), file=path, append=TRUE)
 					if(p==length(dat)) cat("\n", file=path, append=TRUE)
 					else cat(",", file=path, append=TRUE, sep="\n")
 				}
