93457a0a02208a041a8bfcc115fcee197fca5d0d
chgrl <christian.graul@gmail.com>
2015-05-12 22:35:51 +0200

    marker defaults fixed

--- R/styleSingle.R
+++ R/styleSingle.R
@@ -11,7 +11,9 @@ function(col, lwd, alpha, fill, fill.alpha, rad, marker) {
 			else marker[1] <- paste0("\"", marker[1], "\"")
 			if(is.na(marker[2])) marker[2] <- "null"
 			else marker[2] <- paste0("\"", getHex(marker[2]), "\"")
-			style <- append(style, paste0("icon: ", marker[1], ", color: ", marker[2], ", size: \"", marker[3], "\""))
+			if(is.na(marker[3])) marker[3] <- "\"m\""
+			else marker[3] <- paste0("\"", marker[3], "\"")
+			style <- append(style, paste0("icon: ", marker[1], ", color: ", marker[2], ", size: ", marker[3]))
 		} else stop("Markers are specified by 'icon', 'color' and 'size' or just by 'color' value")
 		attr(style, "marker") <- "marker"
 	} else {
